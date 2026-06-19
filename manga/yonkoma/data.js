35f83cdd144554d162a1b84c979e449b1596e864
足本 <00ashimoto@gmail.com>
2026-06-19 23:19:24 +0900

    Add files via upload

---
+++ manga/yonkoma/data.js
@@ -0,0 +1,14 @@
+
+const works = [
+  { file: "01.jpg", title: "IDI 1" },
+  { file: "02.jpg", title: "IDI 2" },
+  { file: "03.jpg", title: "IDI 3" },
+  { file: "04.jpg", title: "IDI 4" },
+  { file: "05.jpg", title: "IDI 5" },
+  { file: "06.jpg", title: "IDI 6" },
+  { file: "07.jpg", title: "IDI 7" },
+  { file: "08.jpg", title: "IDI 8" },
+  { file: "09.jpg", title: "IDI 9" },
+  { file: "10.jpg", title: "IDI 10" },
+  
+];
