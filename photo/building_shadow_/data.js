c77c6b63e2a20cb1a23e02f6e48f0970c8aaa399
足本 <00ashimoto@gmail.com>
2026-06-19 23:20:07 +0900

    Add files via upload

---
+++ photo/building_shadow_/data.js
@@ -0,0 +1,12 @@
+
+const works = [
+  { file: "01.jpg", title: "wall_line 01" },
+  { file: "02.jpg", title: "wall_line 02" },
+  { file: "03.jpg", title: "wall_line 03" },
+  { file: "04.jpg", title: "wall_line 04" },
+  { file: "05.jpg", title: "wall_line 05" },
+  { file: "06.jpg", title: "wall_line 06" },
+  { file: "07.jpg", title: "wall_line 07" },
+  { file: "08.jpg", title: "wall_line 08" },
+
+];
---
+++ photo/wall_line_/data.js
@@ -0,0 +1,9 @@
+
+const works = [
+  { file: "01.jpg", title: "wall_line 01" },
+  { file: "02.jpg", title: "wall_line 02" },
+  { file: "03.jpg", title: "wall_line 03" },
+  { file: "04.jpg", title: "wall_line 04" },
+  { file: "05.jpg", title: "wall_line 05" },
+  { file: "06.jpg", title: "wall_line 06" }
+];
