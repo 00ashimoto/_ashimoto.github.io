1b54069e7dfaa249edaea30c37ff1b25fe99adc7
足本 <00ashimoto@gmail.com>
2026-07-30 19:44:15 +0900

    Update data.js

--- illust/machinomichi/data.js
+++ illust/machinomichi/data.js
@@ -3,4 +3,5 @@ const works = [
   { file: "01.jpg", title: "MACHINOMICHI 1" },
   { file: "02.jpg", title: "MACHINOMICHI 2" },
   { file: "03.jpg", title: "MACHINOMICHI 3" },
+  { file: "04.jpg", title: "MACHINOMICHI 4" },
 ];
